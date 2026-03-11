aa4ef6b98492a5b946eaca3cab716fdf6a09fb03
Meski Abdelilah <meskiabdelilah10@gmail.com>
2026-03-11 14:02:06 +0000

    feat(plat): implement full CRUD operations

--- app/Http/Controllers/Api/PlatController.php
+++ app/Http/Controllers/Api/PlatController.php
@@ -4,8 +4,75 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Plat;
+use Illuminate\Validation\Rule;
+
 
 class PlatController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $plats = $request->user()->plats()->latest()->get();
+
+        return response()->json($plats);
+    }
+
+    public function show(Request $request, $id)
+    {
+        $plat = $request->user()->plats()->findOrFail($id);
+        return response()->json($plat);
+    }
+
+    public function store(Request $request)
+    {
+         $fields = $request->validate([
+            'name' => 
+                [
+                'required',
+                'max:250',
+                'string',
+                Rule::unique('plats')->where('user_id', $request->user()->id)
+                ],
+            'price' => 'required'    
+        ]);
+
+        $plat = $request->user()->plats()->create($fields);
+
+
+        return response()->json([
+            'message' => 'Plat created successfully',
+            'data' => $plat
+        ], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $fields = $request->validate([
+            'name' => 
+                [
+                'required',
+                'max:250',
+                'string',
+                Rule::unique('plats')->where('user_id', $request->user()->id)->ignore($id)
+                ],
+            'price' => 'required'
+        ]);
+        $plat = $request->user()->plats()->findOrFail($id);
+        $plat->update($fields);
+
+        return response()->json([
+            'message' => 'Update successfully',
+            'data' => $plat
+        ], 200);
+    }
+
+    public function destroy(Request $request, $id)
+    {
+        $plat = $request->user()->plats()->findOrFail($id);
+        $plat->delete();
+        return response()->json([
+            'message' => 'Delete successfully',
+            'data' => $plat
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\PlatController;
 use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,11 +17,20 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     /**
-     * les route de crud categories.
+     *CRUD Categories.
      */
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::post('/categories', [CategoryController::class, 'store']);
     Route::put('/categories/{id}', [CategoryController::class, 'update']);
     Route::get('/categories/{id}', [CategoryController::class, 'show']);
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+    /**
+     * CRUD des Plats
+     */
+    Route::get('/plats', [PlatController::class, 'index']);
+    Route::post('/plats', [PlatController::class, 'store']);
+    Route::put('/plats/{id}', [PlatController::class, 'update']);
+    Route::get('/plats/{id}', [PlatController::class, 'show']);
+    Route::delete('/plats/{id}', [PlatController::class, 'destroy']);
 });
